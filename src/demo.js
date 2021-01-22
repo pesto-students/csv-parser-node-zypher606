3bec86e33fb808722d06bfb4a62596456a55fe74
Ashim <ashimrajkonwar@gmail.com>
2021-01-22 16:55:22 +0530

    Add csv to JSON stream

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,14 @@
 const path = require("path");
-const { csvToJsonSync, jsonToCsvSync } = require("./csv-parser");
+const { csvToJsonSync, jsonToCsvSync, csvToJsonStreamAsync } = require("./csv-parser");
 
 const filepathCsv = path.join(__dirname, '../data', 'data.csv');
 const filepathJson = path.join(__dirname, '../data', 'data.json');
 
+console.log("====== CSV to JSON SYNC =====")
 console.log(csvToJsonSync(filepathCsv, true))
+
+console.log("\n\n\n====== JSON to CSV SYNC =====")
 console.log(jsonToCsvSync(filepathJson))
+
+console.log("\n\n\n====== CSV to JSON ASYNC STREAM =====")
+csvToJsonStreamAsync(filepathCsv).pipe(process.stdout)
